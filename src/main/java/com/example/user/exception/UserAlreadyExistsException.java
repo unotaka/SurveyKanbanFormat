ff53789c0860404630369e730a97e7925335e826
uno <t-uno@flight.co.jp>
2026-05-25 16:43:41 +0900

    ファイル削除

--- src/main/java/com/example/user/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
